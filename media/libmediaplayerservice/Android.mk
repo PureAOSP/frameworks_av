1e751a8a521786ab345b288380e085dc18feeaf5
Sharad Sangle <assangle@codeaurora.org>
2013-09-30 15:54:37 +0530

    audio: add support for MSM audio formats
    - Add support for EVRC, QCELP, AMRWB+, and WMA formats
    - Refernece change If110058284c8b3e18c2f242f560fc7748f62081c form jb-mr2
    
    Change-Id: Ie20f2d61f79d2ba41d552718187b7098141e7b8c
    (cherry picked from commit bca561e38853351c0bae15c5d201dbca7f5a027e)
    (cherry picked from commit a11972f505000be397ee3a377ec42bf9e5fef420)
    (cherry picked from commit 7465e6129b291c66dc7741325a90e3725e40e941)

--- media/libmediaplayerservice/Android.mk
+++ media/libmediaplayerservice/Android.mk
@@ -53,18 +53,17 @@ LOCAL_C_INCLUDES :=                                                 \
     $(TOP)/frameworks/native/include/media/openmax                  \
     $(TOP)/external/tremolo/Tremolo                                 \
 
-ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-    ifeq ($(TARGET_QCOM_MEDIA_VARIANT),caf)
-    LOCAL_C_INCLUDES += \
-            $(TOP)/hardware/qcom/media-caf/mm-core/inc
-    else
-    LOCAL_C_INCLUDES += \
-            $(TOP)/hardware/qcom/media/mm-core/inc
-    endif
+ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS), true)
+    LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/media/mm-core/inc
 endif
 
 LOCAL_MODULE:= libmediaplayerservice
 
+ifeq ($(TARGET_ENABLE_QC_AV_ENHANCEMENTS),true)
+    LOCAL_CFLAGS += -DENABLE_QC_AV_ENHANCEMENTS
+    LOCAL_C_INCLUDES += $(TOP)/frameworks/av/include/media
+    LOCAL_C_INCLUDES += $(TOP)/hardware/qcom/media/mm-core/inc
+endif #TARGET_ENABLE_QC_AV_ENHANCEMENTS
 include $(BUILD_SHARED_LIBRARY)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
